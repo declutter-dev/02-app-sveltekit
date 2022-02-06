a472e86638d3c629bdd8b69e515562f5d77ebf80
Christophe R Patraldo <patrouch@gmail.com>
2022-02-06 15:31:26 -0500

    not deploying correctly.  Start 03

--- svelte.config.js
+++ svelte.config.js
@@ -13,15 +13,9 @@ const config = {
 			out: 'build',
 			precompress: false,
 			env: {
-				path: 'SOCKET_PATH',
 				host: 'HOST',
 				port: 'PORT',
-				origin: 'ORIGIN',
-				headers: {
-					protocol: 'PROTOCOL_HEADER',
-					host: 'HOST_HEADER'
 				}
-			}
 		}),
 
 		// hydrate the <div id="svelte"> element in src/app.html
